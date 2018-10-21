3b615bf108d35aea7dda93bfa41541fad91c1dec
Taqueem <taqueem@yahoo.in>
2018-10-21 20:19:43 +0530

    Base Test First Checkin

---
+++ BaseTest/src/main/java/testCases/NewTest.java
@@ -0,0 +1,37 @@
+package testCases;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class NewTest {
+
+	WebDriver driver;
+
+	@Test
+	public void f() {
+
+		System.out.println("Inside the test");
+		System.setProperty("webdriver.gecko.driver",
+				"D:\\Selenium\\Driver\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+		/*		FirefoxOptions options = new FirefoxOptions();
+				options.setBinary(
+						"C:\\Program Files\\Mozilla Version\\55.0\\firefox.exe");
+				options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				options.setAcceptInsecureCerts(true);*/
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+
+		System.out.println("Inside the beforeTest");
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+		System.out.println("Inside the afterTest");
+	}
+}
