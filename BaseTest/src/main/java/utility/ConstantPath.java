93c3f933e092cfafea84bb380e16f15638208305
Jawed <Jawed@172.26.158.43>
2019-02-07 18:15:43 +0800

    fixes paths

--- BaseTest/src/main/java/utility/ConstantPath.java
+++ BaseTest/src/main/java/utility/ConstantPath.java
@@ -3,19 +3,44 @@ public class ConstantPath {
 
 	// public static final String pathGeckoDriver ="C:\\Program Files\\Mozilla
 	// Version\\55.0\\geckodriver.exe";
-	public static final String	pathGeckoDriver		= "D:\\Selenium\\Driver\\geckodriver-v0.19.0-win64\\geckodriver.exe";
+	/*
+	 * public static final String pathGeckoDriver =
+	 * "D:\\Selenium\\Driver\\geckodriver-v0.19.0-win64\\geckodriver.exe";
+	 * 
+	 * public static final String pathFirefoxBinary =
+	 * "‪C:\\\\Program Files\\\\Mozilla Version\\\\55.0\\\\firefox.exe";
+	 * 
+	 * public static final String pathChromeDriver =
+	 * "D:\\Selenium\\Driver\\chromedriver\\chromedriver.exe";
+	 * 
+	 * public static final String pathEdgeDriver =
+	 * "D:\\Selenium\\Driver\\EdgeDriver\\MicrosoftWebDriver.exe";
+	 * 
+	 * public static final String pathIEDriver =
+	 * "D:\\Selenium\\Driver\\IEDriverServer\\IEDriverServer.exe";
+	 * 
+	 * public static final String pathScreenshots = "D:\\Selenium\\ScreenShots\\";
+	 * 
+	 * public static final String pathExtentReports =
+	 * "D:\\Selenium\\ExtentReports\\";
+	 */
+	
+	
+	
+	public static final String	pathGeckoDriver		= "src\\main\\resources\\drivers\\geckodriver.exe";
 
-	public static final String	pathFirefoxBinary	= "‪C:\\\\Program Files\\\\Mozilla Version\\\\55.0\\\\firefox.exe";
+	//public static final String	pathFirefoxBinary	= "‪C:\\\\Program Files\\\\Mozilla Version\\\\55.0\\\\firefox.exe";
 
-	public static final String	pathChromeDriver	= "D:\\Selenium\\Driver\\chromedriver\\chromedriver.exe";
+	public static final String	pathChromeDriver	= "src\\main\\resources\\drivers\\chromedriver.exe";
 
-	public static final String	pathEdgeDriver		= "D:\\Selenium\\Driver\\EdgeDriver\\MicrosoftWebDriver.exe";
+	public static final String	pathEdgeDriver		= "src\\main\\resources\\drivers\\MicrosoftWebDriver.exe";
 
 	public static final String	pathIEDriver		= "D:\\Selenium\\Driver\\IEDriverServer\\IEDriverServer.exe";
 
 	public static final String	pathScreenshots		= "D:\\Selenium\\ScreenShots\\";
 
 	public static final String	pathExtentReports	= "D:\\Selenium\\ExtentReports\\";
+	
 	// public static final String pathScreenshots =
 	// "\\BaseTest\\resources\\testReports\\";
 	//
